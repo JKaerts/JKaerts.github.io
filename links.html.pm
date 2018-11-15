57a54b1603bfe144dfe9cc9c131fb8f6ba2ca85c
Jonas Kaerts <jonaskaerts@gmail.com>
2018-11-15 20:22:25 +0100

    Links.html omgezet naar pollen en index.html herladen

---
+++ links.html.pm
@@ -0,0 +1,23 @@
+#lang pollen
+◊h1{Links}
+◊h2{Youtube-kanalen}
+◊dl{
+    ◊dt{◊a[#:href "https://www.youtube.com/channel/UC2bkHVIDjXS7sgrgjFtzOXQ"]{Engineerguy}}
+    ◊dd{
+    Engineerguy is het Youtubekanaal rond Bill Hammack.
+    In hopen filmpjes legt hij uit hoe alledaagse voorwerpen ontworpen worden en hoe ze werken.
+    De onderwerpen lopen van de microgolfoven tot een harde schijf tot zelfs een luier.
+    Naast Youtube heeft Bill ook een eigen ◊a[#:href "www.engineerguy.com"]{website} met meer info over zijn video's en zijn projecten.
+    }
+
+    ◊dt{◊a[#:href "https://www.youtube.com/channel/UCAL3JXZSzSm8AlZyD3nQdBA"]{Primitive Technology}}
+    ◊dd{
+    Primitive Technology houdt zich bezig met (grote verrassing) primitieve technologie.
+    Dit kanaal is gewijd aan het maken van primitieve hutten en gereedschappen door enkel gebruik te maken van materialen die 'in het wild' te vinden zijn.
+    Video's komen niet heel vaak uit maar ieder exemplaar is uitermate interessant.
+    Iedere video volgt de typische stijl waarin niet gesproken wordt en iedere handeling duidelijk voor zich spreekt.
+    Meer info is ook te vinden in de ◊a[#:href "https://primitivetechnology.wordpress.com/"]{blog}.
+    }
+}
+
+◊a[#:href "./index.html"]{Terug naar af}
