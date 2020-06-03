b4fd81782d8bc33311ae4711062bccb8eedffbbd
Jonas Kaerts <jonas@jkaerts.net>
2020-06-03 20:18:16 +0200

    converted index

--- render.sh
+++ render.sh
@@ -1,1 +1,5 @@
-php templates/documenten.php > documenten.html
+declare -a files=("index" "documenten")
+
+for file in "${files[@]}"
+do
+  php templates/$file.php > $file.html
--- templates/documenten.php
+++ templates/documenten.php
@@ -3,7 +3,7 @@ $title="Schrijfsels"
 include("header.php");
 ?>
 
-<h3 class="topic">Schrijfsels</h3>
+<h3 class="topic"><?= $title ?></h3>
 <ul>
   <li><a href="./subgroups.html">Experiment on gradual exposition (EN)</a></li>
   <li><a href="./tikz_dsl_1.html">Creating a small DSL in TikZ (part 1) (EN)</a></li>
